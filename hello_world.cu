7f5987ad62e9770be70c5f807a8e90463a77a83d
Kali Live user <kali@localhost>
2026-08-07 15:10:25 -0500

    initial commit; hello_world

---
+++ hello_world.cu
@@ -0,0 +1,13 @@
+#include <cuda_runtime.h>
+#include <iostream>
+
+__global__ void hello_world() {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    printf("Hello, World! Thread %d\n", tid);
+}
+
+int main() {
+    hello_world<<<1, 10>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
